72521dce6566b765a3be418d4f8c0afad2ae469e
Lovasoa <maitrejedi@lovasoa.fr>
2025-05-18 16:51:02 +0200

    feat: enhance UI configuration with new badge variants and color theme

--- app.config.ts
+++ app.config.ts
@@ -1,2 +1,23 @@
 export default defineAppConfig({
+  ui: {
+    badge: {
+      compoundVariants: [
+        {
+          color: 'green',
+          variant: 'subtle',
+          class: 'bg-green-50 text-green-600 ring-1 ring-inset ring-green-600/10'
+        },
+        {
+          color: 'red',
+          variant: 'subtle',
+          class: 'bg-red-50 text-red-600 ring-1 ring-inset ring-red-600/10'
+        },
+        {
+          color: 'lovasoa',
+          variant: 'subtle',
+          class: 'bg-lovasoa-50 text-lovasoa-500 ring-1 ring-inset ring-lovasoa-500'
+        }
+      ]
+    },
+  },
 }) 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-  
+
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
+  ui: {
+    theme: {
+      colors: ['green', 'red', 'lovasoa']
+    }
+  },
   modules: [
     // '@nuxt/content',
     '@nuxt/eslint',
